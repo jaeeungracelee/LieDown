ae2bd8f3e1a68ca0e1f5f281f1911c3e02ccd92f
alecishuman <aasitu@gmail.com>
2024-06-22 14:42:19 -0700

    frontend

--- frontend/src/app/questions/page.js
+++ frontend/src/app/questions/page.js
@@ -1,25 +1,50 @@
 "use client";
-import SearchBar from "@/components/SearchBar";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+import Image from "next/image";
+import HomeIcon from "@mui/icons-material/Home";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import Link from "next/link";
+
+export default function Questions() {
+  const intro =
+    "H i there! We are just going to have you answer a few questions about this meeting.";
+  const [curTyping, setCurTyping] = useState("");
+
+  const typing = (text) => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setCurTyping((prev) => prev + text.charAt(i)); // Use charAt(i) to get one character at a time
+      i++;
+      if (i >= text.length) {
+        // Adjusted condition to check when i reaches text.length
+        clearInterval(interval);
+      }
+    }, 50);
+  };
+
+  useEffect(() => {
+    typing(intro);
+  }, []);
+
   return (
-    <main>
-      <div className="absolute bottom-4 right-4 text-xs">
-        *Please note that we do not support any unethical activities
-      </div>
-      <div className="flex flex-col gap-6 font-semibold absolute top-[37vh] left-[10vw]">
-        <div className="text-[var(--primary-color)] text-6xl">Lie Down</div>
-        <div className="flex flex-row gap-4">
-          <div className="flex text-3xl font-normal items-end text-[var(--secondary-color)]">
-            while you
-          </div>{" "}
-          <div className="text-6xl min-w-[600px] text-[var(--secondary-color)]">
-            Stand Up
+    <div className="w-full h-[100vh] flex flex-col items-center bg-black">
+      <Link
+        className="absolute top-6 left-6 border-2 border-[var(--primary-color)] p-[6px] rounded-2xl"
+        href="/"
+      >
+        <KeyboardReturnIcon
+          sx={{ color: "var(--primary-color)", fontSize: 32 }}
+        />
+      </Link>
+      <div className="flex flex-col w-1/2 h-full pt-8 pb-4">
+        <div className="flex flex-col gap-2">
+          <div className="font-semibold text-[var(--primary-text)]">
+            Engine:
           </div>
+          <div className="bg-[var(--primary-bg)] rounded-lg">{curTyping}</div>
         </div>
-        <SearchBar />
       </div>
-    </main>
+    </div>
   );
 }
